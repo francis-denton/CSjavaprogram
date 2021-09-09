368d1d096866002d43f7d0c3deb5bf53956fa371
Unabatedlynx5 <Frankrobertdenton@gmail.com>
2021-09-09 09:38:02 -0500

    Kotlin

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
     public static void main(String[] args) {
         int coolNumber=7;  
         System.out.println(coolNumber);
+        System.out.println("Kotlin is a better Java Language");
     }
      
 }
