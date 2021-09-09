c80e295c2344114c5763e17a756acb6776884930
Unabatedlynx5 <Frankrobertdenton@gmail.com>
2021-09-09 11:25:52 -0500

    Input

---
+++ input.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class input {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter name, age and salary:");
+
+    // String input
+    System.out.print("Name:");
+    String name = myObj.nextLine();
+
+    // Numerical input
+    System.out.print("Age:");
+    int age = myObj.nextInt();
+    System.out.print("Salary:");
+    double salary = myObj.nextDouble();
+
+    // Output input by user
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+  }
+}
+/*
+nextBoolean()	Reads a boolean value from the user
+nextByte()	Reads a byte value from the user
+nextDouble()	Reads a double value from the user
+nextFloat()	Reads a float value from the user
+nextInt()	Reads a int value from the user
+nextLine()	Reads a String value from the user
+nextLong()	Reads a long value from the user
+nextShort()	Reads a short value from the user
+*/
